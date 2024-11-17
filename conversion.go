dfb2d31ebf530104a56576ca1418789207a3aeb2
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2024-11-18 00:02:17 +0100

    chore: simplify code
    One code to rule them all.
    
    This code is pretty similar to the one @ldemailly provided for
    https://github.com/fortio/safecast
    
    We worked at the same time on the same code, we came to distinct solutions.
    His one was better and simpler than mine, so now I'm simply switching to
    something highly inspired from his code.
    
    Co-Authored-By: Laurent Demailly <ldemailly@gmail.com>

--- conversion.go
+++ conversion.go
@@ -5,154 +5,113 @@
 
 package safecast
 
-import "math"
+import (
+	"math"
+)
+
+func convertFromNumber[NumOut Number, NumIn Number](orig NumIn) (converted NumOut, err error) {
+	converted = NumOut(orig)
+
+	errBoundary := ErrExceedMaximumValue
+	boundary := getUpperBoundary(converted)
+	if negative(orig) {
+		errBoundary = ErrExceedMinimumValue
+		boundary = getLowerBoundary(converted)
+	}
+
+	if !sameSign(orig, converted) {
+		return 0, Error{
+			value:    orig,
+			err:      errBoundary,
+			boundary: boundary,
+		}
+	}
+
+	base := orig
+	switch f := any(orig).(type) {
+	case float64:
+		base = NumIn(math.Trunc(f))
+	case float32:
+		base = NumIn(math.Trunc(float64(f)))
+	}
+
+	if NumIn(converted) == base {
+		return converted, nil
+	}
+
+	return 0, Error{
+		value:    orig,
+		err:      errBoundary,
+		boundary: boundary,
+	}
+}
 
 // ToInt attempts to convert any [Number] value to an int.
 // If the conversion results in a value outside the range of an int,
 // an [ErrConversionIssue] error is returned.
 func ToInt[T Number](i T) (int, error) {
-	if err := checkUpperBoundary(i, int(math.MaxInt)); err != nil {
-		return 0, err
-	}
-
-	if err := checkLowerBoundary(i, int(math.MinInt)); err != nil {
-		return 0, err
-	}
-
-	return int(i), nil
+	return convertFromNumber[int](i)
 }
 
 // ToUint attempts to convert any [Number] value to an uint.
 // If the conversion results in a value outside the range of an uint,
 // an [ErrConversionIssue] error is returned.
 func ToUint[T Number](i T) (uint, error) {
-	if err := checkLowerBoundary(i, uint(0)); err != nil {
-		return 0, err
-	}
-
-	if err := checkUpperBoundary(i, uint(math.MaxUint)); err != nil {
-		return 0, err
-	}
-
-	return uint(i), nil
+	return convertFromNumber[uint](i)
 }
 
 // ToInt8 attempts to convert any [Number] value to an int8.
 // If the conversion results in a value outside the range of an int8,
 // an [ErrConversionIssue] error is returned.
 func ToInt8[T Number](i T) (int8, error) {
-	if err := checkUpperBoundary(i, int8(math.MaxInt8)); err != nil {
-		return 0, err
-	}
-
-	if err := checkLowerBoundary(i, int8(math.MinInt8)); err != nil {
-		return 0, err
-	}
-
-	return int8(i), nil
+	return convertFromNumber[int8](i)
 }
 
 // ToUint8 attempts to convert any [Number] value to an uint8.
 // If the conversion results in a value outside the range of an uint8,
 // an [ErrConversionIssue] error is returned.
 func ToUint8[T Number](i T) (uint8, error) {
-	if err := checkLowerBoundary(i, uint8(0)); err != nil {
-		return 0, err
-	}
-
-	if err := checkUpperBoundary(i, uint8(math.MaxUint8)); err != nil {
-		return 0, err
-	}
-
-	return uint8(i), nil
+	return convertFromNumber[uint8](i)
 }
 
 // ToInt16 attempts to convert any [Number] value to an int16.
 // If the conversion results in a value outside the range of an int16,
 // an [ErrConversionIssue] error is returned.
 func ToInt16[T Number](i T) (int16, error) {
-	if err := checkUpperBoundary(i, int16(math.MaxInt16)); err != nil {
-		return 0, err
-	}
-
-	if err := checkLowerBoundary(i, int16(math.MinInt16)); err != nil {
-		return 0, err
-	}
-
-	return int16(i), nil
+	return convertFromNumber[int16](i)
 }
 
 // ToUint16 attempts to convert any [Number] value to an uint16.
 // If the conversion results in a value outside the range of an uint16,
 // an [ErrConversionIssue] error is returned.
 func ToUint16[T Number](i T) (uint16, error) {
-	if err := checkLowerBoundary(i, uint16(0)); err != nil {
-		return 0, err
-	}
-
-	if err := checkUpperBoundary(i, uint16(math.MaxUint16)); err != nil {
-		return 0, err
-	}
-
-	return uint16(i), nil
+	return convertFromNumber[uint16](i)
 }
 
 // ToInt32 attempts to convert any [Number] value to an int32.
 // If the conversion results in a value outside the range of an int32,
 // an [ErrConversionIssue] error is returned.
 func ToInt32[T Number](i T) (int32, error) {
-	if err := checkUpperBoundary(i, int32(math.MaxInt32)); err != nil {
-		return 0, err
-	}
-
-	if err := checkLowerBoundary(i, int32(math.MinInt32)); err != nil {
-		return 0, err
-	}
-
-	return int32(i), nil
+	return convertFromNumber[int32](i)
 }
 
 // ToUint32 attempts to convert any [Number] value to an uint32.
 // If the conversion results in a value outside the range of an uint32,
 // an [ErrConversionIssue] error is returned.
 func ToUint32[T Number](i T) (uint32, error) {
-	if err := checkLowerBoundary(i, uint32(0)); err != nil {
-		return 0, err
-	}
-
-	if err := checkUpperBoundary(i, uint32(math.MaxUint32)); err != nil {
-		return 0, err
-	}
-
-	return uint32(i), nil
+	return convertFromNumber[uint32](i)
 }
 
 // ToInt64 attempts to convert any [Number] value to an int64.
 // If the conversion results in a value outside the range of an int64,
 // an [ErrConversionIssue] error is returned.
 func ToInt64[T Number](i T) (int64, error) {
-	if err := checkLowerBoundary(i, int64(math.MinInt64)); err != nil {
-		return 0, err
-	}
-
-	if err := checkUpperBoundary(i, int64(math.MaxInt64)); err != nil {
-		return 0, err
-	}
-
-	return int64(i), nil
+	return convertFromNumber[int64](i)
 }
 
 // ToUint64 attempts to convert any [Number] value to an uint64.
 // If the conversion results in a value outside the range of an uint64,
 // an [ErrConversionIssue] error is returned.
 func ToUint64[T Number](i T) (uint64, error) {
-	if err := checkLowerBoundary(i, uint64(0)); err != nil {
-		return 0, err
-	}
-
-	if err := checkUpperBoundary(i, uint64(math.MaxUint64)); err != nil {
-		return 0, err
-	}
-
-	return uint64(i), nil
+	return convertFromNumber[uint64](i)
 }
