214bd73f1af9074c5aad668f22a7355392699751
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2024-12-18 00:33:32 +0100

    doc: improve documentation
    The struct of the error is no longer exported.
    
    It helps to provide a better documentation, even if it's a breaking change.

---
+++ doc.go
@@ -0,0 +1,6 @@
+package safecast
+
+// Package go-safecast solves the type conversion issues in Go
+//
+// In Go, integer type conversion can lead to unexpected behavior and errors if not handled carefully.
+// Issues can happen when converting between signed and unsigned integers, or when converting to a smaller integer type.
