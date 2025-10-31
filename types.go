a68adaead3683bf45273d4ff2479417416ba12c0
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2025-10-31 01:29:35 +0100

    chore: remove the types that are not used
    These types were public and are now removed. They were only used to define
    Number type which is now defined differently.
    
    This allows us to simplify the documentation of the package.
    
    This is a BREAKING CHANGE

--- types.go
+++ types.go
@@ -4,34 +4,11 @@ import (
 	"math"
 )
 
-// This files is highly inspired from https://pkg.go.dev/golang.org/x/exp/constraints
-// I didn't import it as it would have added an unnecessary dependency
-
-// Signed is a constraint for all signed integers: int, int8, int16, int32, and int64 types.
-type Signed interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64
-}
-
-// Unsigned is a constraint for all unsigned integers: uint, uint8, uint16, uint32, and uint64 types.
-// TODO: support uintpr
-type Unsigned interface {
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
-}
-
-// Integer is a constraint for the all unsigned and signed integers
-type Integer interface {
-	Signed | Unsigned
-}
-
-// Float is a constraint for the float32 and float64 types.
-type Float interface {
-	~float32 | ~float64
-}
-
 // Number is a constraint for all integers and floats
-// TODO: consider using complex, but not sure there is a need
 type Number interface {
-	Integer | Float
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64
+
+	// TODO(ccoVeille): consider using complex, but not sure if there is a need
 }
 
 func isNegative[T Number](t T) bool {
